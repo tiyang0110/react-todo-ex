047582530ef6a1dd9e456061691f7e5cad36ef5b
tiyang0110 <tiyang0110@douzone.com>
2022-07-13 11:37:01 +0900

    React Todo Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,4 @@
 import { createGlobalStyle } from "styled-components";
-import Router from "./Router";
-import { ReactQueryDevtools } from 'react-query/devtools';
-import { ThemeProvider } from 'styled-components';
-import { darkTheme, lightTheme } from './theme';
-import { useState } from "react";
-import { useRecoilValue } from "recoil";
-import { isDarkAtom } from "./atoms";
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -69,15 +62,9 @@ const GlobalStyle = createGlobalStyle`
 
 function App() {
 
-  const isDark = useRecoilValue(isDarkAtom);
-
   return (
     <>
-      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-        <GlobalStyle />
-        <Router />
-        <ReactQueryDevtools initialIsOpen={true}/>
-      </ThemeProvider>
+      <GlobalStyle />
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import { darkTheme } from './theme';
 
 const rootEl = document.getElementById("root");
 if (!rootEl) throw new Error('root is undefined');
 const root = ReactDOM.createRoot(rootEl);
 
-const queryClient = new QueryClient();
 
 root.render(
   <RecoilRoot>
-    <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={darkTheme}>      
       <App />
-    </QueryClientProvider>
+    </ThemeProvider>
   </RecoilRoot>
 );
